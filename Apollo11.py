eecf3bc5acf311a7732e9f8678aa77b123bc52de
Danitcas <141188134+Danitcas@users.noreply.github.com>
2024-01-28 00:56:56 -0500

    n

--- Apollo11.py
+++ Apollo11.py
@@ -3,6 +3,7 @@ import random
 from tqdm import tqdm
 import time
 from typing import NoReturn
+import argparse
 
 # Importación de Clases y métodos
 from Archivos.Configuracion.Configuracion import Configuracion
@@ -22,6 +23,20 @@ procesador = Generador()
 
 
 def main() -> NoReturn:
+    parser = argparse.ArgumentParser(description='INGRESE LA OPCION QUE DESEA MODIFICAR.')
+    parser.add_argument('--ciclo', type=float, help='Ciclo de tiempo en segundos')
+    parser.add_argument('--min', type=float, help='Cambiar cantidad mínima de archivos a generar')
+    parser.add_argument('--max', type=float, help='Cambiar cantidad máxima de archivos a generar')
+    args = parser.parse_args()
+    if args.ciclo is not None:
+        config.cambiar_ciclo(args.ciclo)
+        print('ciclo cambiado')
+    elif args.min is not None:
+        config.cambiar_min_archivos(args.min)
+    elif args.max is not None:
+        config.cambiar_max_archivos(args.max)
+    else:
+        print("No se suministraron opciones")
 
 
     try:
--- Archivos/InterfazUsuario/Interfaces.py
+++ Archivos/InterfazUsuario/Interfaces.py
@@ -50,51 +50,40 @@ class Interfaces:
             logging.error("Error al mostrar el reporte", exc_info=True)
 
     @staticmethod
-    def menu_alternativo()-> None:
-        # Las siguientes operaciones se realizan en el módulo de configuración, se asume que están correctamente ajustadas con logging.
-        opcion =0
-        while ( not (opcion == 7)):
-            opcion = input ("Que informacióm desea cambiar?\n 1. Ciclo de  tiempo (s)\n 2. Eliminar Dispositivos \n 3. Añadir un nuevo dispositivo \n 4. Cambiar cantidad minima de archivos a generar \n 5. Cambiar cantidad maxima de archivos a generar \n 6. Crear copia del ultimo reporte disponible \n 7. Seguir a la generacion de archivos \n" )
-            if opcion == "1":
-                ciclo = input("Ingrese nuevo ciclo de tiempo: ")
-                Config.cambiar_ciclo(float(ciclo))
-            elif opcion == "2":
-                Config.menu_eliminar_dispositivo()
-            elif opcion == "3":
-                Config.menu_nuevo_dispositivo()
-            elif opcion == "4":
-                Config.menu_cambiar_min_archivos()
-            elif opcion == "5":
-                Config.menu_cambiar_max_archivos()
-            elif opcion == "6":
-                Config.Crear_copia()
-            elif opcion == "7":
-                break
-            else:
-                print("Opción errónea, inténtelo otra vez.")
-                logging.info("Opción errónea, inténtelo otra vez.")
-
-
-    @staticmethod
-    def menu_inicial() -> None:
-        """Genera el menú inicial que se ve desde consola."""
+    def menu_inicial()-> None:
         Interfaces.bienvenido()
         Interfaces.apollo11()
         #Config.dispositivos()
         Config.ciclo()
-    
-        parser = argparse.ArgumentParser(description='INGRESE LA OPCION QUE DESEA MODIFICAR.')
-        parser.add_argument('--ciclo', type= float, help='Ciclo de tiempo en segundos')
-        parser.add_argument('--eliminar', action='store_true', help='Eliminar dispositivos')
-        parser.add_argument('--nuevo', action='store_true', help='Añadir un nuevo dispositivo')
-        parser.add_argument('--min', type=float, help='Cambiar cantidad mínima de archivos a generar')
-        parser.add_argument('--max', type=float, help='Cambiar cantidad máxima de archivos a generar')
-        parser.add_argument('--crear-copia', action='store_true', help='Crear copia del último reporte disponible')
-
+        # Las siguientes operaciones se realizan en el módulo de configuración, se asume que están correctamente ajustadas con logging.
         nombre = input(" ¿DESEA CONTINUAR CON LOS ANTERIORES DATOS? [y/n]: ")
         if nombre.lower() == "n":
-            parser.print_help()
-            print("Recuerde utilizar el comando: python Apollo11.py --opcion valor_cambiado, para modificar valores desde consola.\n\nDe lo contrario, continue con el siguiente Menu:")
-            logging.info("Recuerde utilizar el comando: python Apollo11.py --opcion valor_cambiado, para modificar valores desde consola.\n\nDe lo contrario, continue con el siguiente Menu:")
-            # Interfaces.menu_alternativo()
+            opcion =0
+            while ( not (opcion == 7)):
+                opcion = input ("Que informacióm desea cambiar?\n 1. Ciclo de  tiempo (s)\n 2. Eliminar Dispositivos \n 3. Añadir un nuevo dispositivo \n 4. Cambiar cantidad minima de archivos a generar \n 5. Cambiar cantidad maxima de archivos a generar \n 6. Crear copia del ultimo reporte disponible \n 7. Seguir a la generacion de archivos \n" )
+                if opcion == "1":
+                    ciclo = input("Ingrese nuevo ciclo de tiempo: ")
+                    Config.cambiar_ciclo(float(ciclo))
+                    break
+                elif opcion == "2":
+                    Config.menu_eliminar_dispositivo()
+                    break
+                elif opcion == "3":
+                    Config.menu_nuevo_dispositivo()
+                    break
+                elif opcion == "4":
+                    Config.menu_cambiar_min_archivos()
+                    break
+                elif opcion == "5":
+                    Config.menu_cambiar_max_archivos()
+                    break
+                elif opcion == "6":
+                    Config.Crear_copia()
+                    break
+                elif opcion == "7":
+                    break
+                else:
+                    print("Opción errónea, inténtelo otra vez.")
+                    logging.info("Opción errónea, inténtelo otra vez.")
+
                     
